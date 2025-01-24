3f77364de8a52dc77ad95e26652d2ccf0f3bb7ac
liran906 <liran900620@gmail.com>
2025-01-24 12:41:28 +0800

    more neetcode

---
+++ neetcode/010Reverse Linked List.py
@@ -0,0 +1,23 @@
+# Reverse Linked List
+# https://neetcode.io/problems/reverse-a-linked-list
+# e
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseList(self, head):
+        currentN = head
+        prevN = None
+        while currentN and currentN.next:
+            nextN = currentN.next
+            currentN.next = prevN
+            prevN = currentN
+            currentN = nextN
+        if currentN:
+            currentN.next = prevN
+        head = currentN
+        return head
