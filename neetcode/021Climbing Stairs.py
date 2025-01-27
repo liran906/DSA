b29fbf6ba7888c2ab5fbf4651e1f29573f313861
liran906 <liran900620@gmail.com>
2025-01-27 20:45:55 +0800

    more neetcode

--- neetcode/021Climbing Stairs.py
+++ neetcode/021Climbing Stairs.py
@@ -23,10 +23,10 @@ class Solution:
             self.memo[i] = self.climbStairs(i - 1) + self.climbStairs(i - 2)
         return self.memo[n]
 
-# Math? time: O(n), space: O(1)
+# Dynamic Programming (optimize space) (Bottom-Up) time: O(n), space: O(1)
 class Solution:
     def climbStairs(self, n):
-        currentStep = 1
+        currentStep = 1 # only save to last 2 steps to save space
         previousStep = 1
         for i in range(n - 1): # started from n = 1 (instead of 0), so only range to n - 1
             currentStep, previousStep = currentStep + previousStep, currentStep
