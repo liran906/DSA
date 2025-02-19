3aa4ee824368988becef7cac9bb1141b29502174
liran906 <liran900620@gmail.com>
2025-02-19 23:51:01 +0800

    more neetcode

---
+++ neetcode/041Pow(x, n).py
@@ -0,0 +1,12 @@
+# Pow(x, n)
+# https://neetcode.io/problems/pow-x-n
+# m
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        res = 1
+        for i in range(abs(n)):
+            res *= x
+        if n < 0:
+            res = 1 / res
+        return res
