cce6eb4773f8d8e9f20b5e62ddadba7bdab6cc37
liran906 <liran900620@gmail.com>
2025-04-17 14:42:33 +0800

    lc

--- Leetcode/gotest.go
+++ Leetcode/gotest.go
@@ -5,45 +5,7 @@ import (
 )
 
 func main() {
-	h := "aabaaabaaac"
-	n := "aabaaac"
-	fmt.Println(strStr(h, n))
-}
-
-func strStr(haystack string, needle string) int {
-	n := len(needle)
-	if n == 0 {
-		return 0
-	} else {
-		next := getNext(needle)
-		hi, ni := 0, 0
-		for hi < len(haystack) {
-			for hi < len(haystack) && ni < n && haystack[hi] == needle[ni] {
-				hi++
-				ni++
-			}
-			if ni == n {
-				return hi - n
-			} else if ni > 0 {
-				ni = next[ni-1]
-				continue
-			}
-			hi++
-		}
+	for range 3 {
+		fmt.Println("fds")
 	}
-	return -1
-}
-
-func getNext(str string) (next []int) {
-	s := []byte(str)
-	next = make([]int, len(str))
-	for i, j := 0, 1; j < len(next); j++ {
-		if s[i] == s[j] {
-			i++
-		} else {
-			i = 0
-		}
-		next[j] = i
-	}
-	return
 }
