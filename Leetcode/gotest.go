05de68d999f6092950ecb8b0d0f2f656089d1385
liran906 <liran900620@gmail.com>
2025-05-08 14:44:36 +0800

    lc

--- Leetcode/gotest.go
+++ Leetcode/gotest.go
@@ -11,41 +11,46 @@ type TreeNode struct {
 }
 
 func main() {
-	t1 := &TreeNode{Val: 5}
-	t2 := &TreeNode{Val: 4, Right: t1}
-	t3 := &TreeNode{Val: 3}
-	t4 := &TreeNode{Val: 2, Left: t2, Right: t3}
-	t5 := &TreeNode{Val: 1}
-	t6 := &TreeNode{Val: 0, Left: t4, Right: t5}
+	// t1 := &TreeNode{Val: 5}
+	// t2 := &TreeNode{Val: 4, Right: t1}
+	// t3 := &TreeNode{Val: 3}
+	// t4 := &TreeNode{Val: 2, Left: t2, Right: t3}
+	// t5 := &TreeNode{Val: 1}
+	// t6 := &TreeNode{Val: 0, Left: t4, Right: t5}
+	inorder := []int{4, 2, 1, 7, 5, 3, 8, 6, 9}
+	postorder := []int{4, 2, 7, 5, 8, 9, 6, 3, 1}
 
-	fmt.Println(levelOrder_2(t6))
+	root := buildTree(inorder, postorder)
+	preorder(root)
 }
 
-func levelOrder_2(root *TreeNode) (res [][]int) {
+func buildTree(inorder []int, postorder []int) *TreeNode {
+	dict := make(map[int]int)
+	for i, v := range inorder {
+		dict[v] = i
+	}
+	var helper func(int, int) *TreeNode
+	helper = func(l, r int) *TreeNode {
+		if l > r {
+			return nil
+		}
+		rootVal := postorder[r]
+		rootIndex := dict[rootVal]
+		root := &TreeNode{Val: rootVal}
+
+		root.Left = helper(l, rootIndex-1)
+		root.Right = helper(rootIndex, r-1)
+
+		return root
+	}
+	return helper(0, len(postorder)-1)
+}
+
+func preorder(root *TreeNode) {
 	if root == nil {
 		return
 	}
-
-	queue := []*TreeNode{root}
-	var node *TreeNode
-	var level []int
-
-	for len(queue) > 0 {
-		levelSize := len(queue)
-		level = []int{}
-		for range levelSize {
-			node = queue[0]
-			queue = queue[1:]
-			level = append(level, node.Val)
-
-			if node.Left != nil {
-				queue = append(queue, node.Left)
-			}
-			if node.Right != nil {
-				queue = append(queue, node.Right)
-			}
-		}
-		res = append(res, level)
-	}
-	return
+	fmt.Println(root.Val)
+	preorder(root.Left)
+	preorder(root.Right)
 }
